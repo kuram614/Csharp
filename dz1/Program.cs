b6081b6b4b2a102dfa190300833febd4b6cd88a4
kURAMA <takzhekazhe@gmail.com>
2023-10-25 17:19:15 +0300

    Написали код для решения домашней задачи №2 к семинару №1з

---
+++ dz1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Int32.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Int32.Parse(Console.ReadLine());
+
+int max = a;
+int min = b;
+
+if (a > b){
+    Console.WriteLine("Максимальное число: ");
+    Console.WriteLine(max);
+    Console.WriteLine("Минимальное число: ");
+    Console.WriteLine(min);
+}else if (a < b){
+    Console.WriteLine("Максимальное число: ");
+    Console.WriteLine(min);
+    Console.WriteLine("Минимальное число: ");
+    Console.WriteLine(max);
+}

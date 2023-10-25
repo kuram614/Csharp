d8dd6f59007e03fbee0e1cd7b93a6324187179f8
kURAMA <takzhekazhe@gmail.com>
2023-10-25 17:56:01 +0300

    Написали код для решения домашней задачи №4 к семинару №1

---
+++ dz2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Int32.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Int32.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int c = Int32.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b > max){
+    max = b;
+}if (c > max){
+    max = c;
+}
+Console.Write("Максимальным числом является: ");
+Console.Write(max);

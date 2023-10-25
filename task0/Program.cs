5a599a9a56cce68fd5f083b4358d56d5d19b752a
kURAMA <takzhekazhe@gmail.com>
2023-10-25 14:50:20 +0300

    added code square number

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное само на себя)
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+// Console.Write("Введите число: ");
+// string number = Console.ReadLine();
+// int result = int.Parse(number);
+// Console.WriteLine(result * result);
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number * number);

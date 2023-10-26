b697d64265b831ce1eb2ede22c5e58b2994b64d5
kURAMA <takzhekazhe@gmail.com>
2023-10-26 14:35:05 +0300

    Поиск макс

---
+++ lection2/Program.cs
@@ -0,0 +1,26 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 12;
+int b1 = 23;
+int c1 = 3431;
+int a2 = 512;
+int b2 = 56;
+int c2 = 670;
+int a3 = 78;
+int b3 = 12575;
+int c3 = 90;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);

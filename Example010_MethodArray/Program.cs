065e6abf4a26d53b981e29090072070a41451a15
kURAMA <takzhekazhe@gmail.com>
2023-10-26 15:30:53 +0300

    Поиск индекса заданного значения в массиве

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 15, 24, 36, 47, 85, 96, 337, 118, 555, 36 };
+
+int n = array.Length;
+int find = 36;
+
+int index = 0;
+
+while(index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++; //index = index + 1;
+}

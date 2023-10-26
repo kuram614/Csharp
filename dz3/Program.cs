b02f3649f5b5badaef69f78a46892eaf06256c4f
kURAMA <takzhekazhe@gmail.com>
2023-10-26 11:42:29 +0300

    Напиали код для решения домашней задачи №6 к семинару №1

---
+++ dz3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int x = Int32.Parse(Console.ReadLine());
+
+if (x % 2 == 0){
+    Console.Write("Число четное");
+}else{
+    Console.Write("Число нечетное");
+}

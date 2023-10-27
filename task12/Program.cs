b6be17779bec7b65e842005ad8200505dc6fab65
kURAMA <takzhekazhe@gmail.com>
2023-10-27 13:52:58 +0300

    Написали код для решения третьей задачи семинара №2

---
+++ task12/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int num1 = number % 7;
+int num2 = number % 23;
+
+if (num1 == 0 && num2 == 0)
+{
+    Console.Write("yes");
+}
+else
+{
+    Console.Write("no");
+}

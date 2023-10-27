5dd663bbafe618e452d798ae47ea6674f05f1b68
kURAMA <takzhekazhe@gmail.com>
2023-10-27 14:33:25 +0300

    Написали код для решения четвертой задачи семинара №2

---
+++ task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int res1 = num1 % (num2*num2);
+int res2 = num2 % (num1*num1);
+
+if(res1 == 0 || res2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}

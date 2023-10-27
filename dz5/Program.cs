63a5381917d33b4ff850995d193250e0bc6f81e5
kURAMA <takzhekazhe@gmail.com>
2023-10-27 15:59:29 +0300

    Записали эталонное решение

--- dz5/Program.cs
+++ dz5/Program.cs
@@ -5,17 +5,35 @@
 // 782 -> 8
 // 918 -> 1
 
-Console.Write("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int secondDigit = number / 10 % 10;
+// Console.Write("Введите трехзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int secondDigit = number / 10 % 10;
 
-if(number < 100 || number >= 1000)
+// if(number < 100 || number >= 1000)
+// {
+//     Console.WriteLine("Вы ввели не трехзначное число, пожалуйста повторите ввод");
+//     return;
+// }
+// else
+// {
+//     Console.WriteLine("Введенное число: " + number);
+//     Console.WriteLine("Вторая цифра: " + secondDigit);
+// }
+
+int Promt(string meassage)
+{
+    System.Console.Write(meassage);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Promt("Введите трехзначное число: ");
+if (number < 100 || number >= 1000)
 {
     Console.WriteLine("Вы ввели не трехзначное число, пожалуйста повторите ввод");
     return;
 }
-else
-{
-    Console.WriteLine("Введенное число: " + number);
-    Console.WriteLine("Вторая цифра: " + secondDigit);
-}
+Console.WriteLine($"Введенное число `{number}`");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра `{secondRank}`");
---
+++ dz6/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int thirdDigit = number / 10 % 10;
+
+if(number < 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine("Введенное число: " + number);
+    Console.WriteLine("Вторая цифра: " + secondDigit);
+}

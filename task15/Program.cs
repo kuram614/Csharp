d328b3e91eaca8b0afe2f1019ba70dc82fd4fa1c
kURAMA <takzhekazhe@gmail.com>
2023-11-01 16:43:28 +0300

    Поиск расстояния между двумя точками в 2D пространстве

--- task15/Program.cs
+++ task15/Program.cs
@@ -3,3 +3,23 @@
 
 // A (3,6); B (2,1) -> 5,09
 // A (7,-5); B (1,-1) -> 7,21
+
+int Input(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+double Dist(int ax, int ay, int bx, int by)
+{
+    double result = Math.Sqrt(Math.Pow((bx - ax), 2) + Math.Pow((by - ay), 2));
+    return result;
+}
+
+int ax = Input("Введите Ax: "),
+    ay = Input("Введите Ay: "),
+    bx = Input("Введите Bx: "),
+    by = Input("Введите By: ");
+
+System.Console.WriteLine($"Distance beetwen = {Math.Round(Dist(ax, ay, bx, by),2)}"); 
